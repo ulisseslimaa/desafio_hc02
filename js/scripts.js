ba30745ce4ffc93b89a98c5ee5e4e776b3b53583
ulisseslimaa <ulisses.lima741@gmail..com>
2021-07-25 19:18:18 -0300

    desafio02

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+class Validator {
+  // método para exibir inputs que são necessários
+  required(input) {
+
+    let inputValue = input.value;
+
+    if(inputValue === '') {
+      let errorMessage = `Este campo é obrigatório`;
+
+      this.printMessage(input, errorMessage);
+    }
+
+  }
+
+}
